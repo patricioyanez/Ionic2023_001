75895d14f7cf4b4dc3c9b407347d3649f95d35cd
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-16 14:17:45 -0400

    Clase 1

--- MiAplicacion/src/app/app-routing.module.ts
+++ MiAplicacion/src/app/app-routing.module.ts
@@ -3,17 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'uno',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'dos',
     loadChildren: () => import('./dos/dos.module').then( m => m.DosPageModule)
+  },
+  {
+    path: 'uno',
+    loadChildren: () => import('./uno/uno.module').then( m => m.UnoPageModule)
   },
 ];
 
