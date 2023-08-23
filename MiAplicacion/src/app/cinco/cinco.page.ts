7301a06b0c954ef7ce07a13f6d9907ca887d1d2b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-23 14:22:34 -0400

    clase 3

--- MiAplicacion/src/app/cinco/cinco.page.ts
+++ MiAplicacion/src/app/cinco/cinco.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cinco',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cinco.page.scss'],
 })
 export class CincoPage implements OnInit {
-  public alertButtons = ['Aceptar'];
-  constructor() { }
+  public alertButtons = ['Aceptalo'];
+  numero : number;
+  numero2: number = 100;
+
+  constructor(private alertController: AlertController) {
+    this.numero = 0;
+   }
 
   ngOnInit() {
   }
+// type script
+  async alerta(){
+    const alert = await this.alertController.create({
+      header    : 'Alerta usando AlertController',
+      subHeader : 'SubTitulo',
+      message   : "Ejemplo de mensaje de alerta usando la clase AlertController",
+      buttons   : ['Ok'],      
+    });
+    // visualizar la alerta
+    await alert.present();
+  }
 
+  async incrementar()
+  {
+    this.numero += 1;
+  }
 }
