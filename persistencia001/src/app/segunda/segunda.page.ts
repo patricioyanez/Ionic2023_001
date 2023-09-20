cb6513a2e6d28df21e6ea3bb4d5f2b558c7e3625
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-20 14:18:49 -0300

    Unidad 2

---
+++ persistencia001/src/app/segunda/segunda.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-segunda',
+  templateUrl: './segunda.page.html',
+  styleUrls: ['./segunda.page.scss'],
+})
+export class SegundaPage implements OnInit {
+  valorParametro:any;
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.valorParametro = this.route.snapshot.paramMap.get("numerito");
+  }
+
+}
