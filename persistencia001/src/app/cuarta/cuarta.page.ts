8e1fde5c6ad901ca6f6b8a9c6ac25288bc9f24e8
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-02 13:56:21 -0300

    update

--- persistencia001/src/app/cuarta/cuarta.page.ts
+++ persistencia001/src/app/cuarta/cuarta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 // instalar npm install @ionic/storage-angular
 import {Storage} from '@ionic/storage-angular';
 
@@ -10,7 +11,8 @@ import {Storage} from '@ionic/storage-angular';
 export class CuartaPage implements OnInit {
   id:string = '';
   nombre:string = '';
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage,
+              private alert: AlertController) { }
 
   ngOnInit() {
     this.storage.create();
@@ -19,11 +21,30 @@ export class CuartaPage implements OnInit {
   agregar()
   {
     //this.storage.set('1', 'Valor a guardar');
-    this.storage.set(this.id, this.nombre);
     // Ejercicio 1:
     // Validar que nombre no este vacio
     // enviar alert si está vacio.
-    this.id = '';
-    this.nombre = '';
+
+    if(!this.id)
+      this.mensajeError("Falta el Id");
+    else if(!this.nombre)
+      this.mensajeError("Falta nombre");
+    else
+    {
+      this.storage.set(this.id, this.nombre);
+      this.id = '';
+      this.nombre = '';
+    }
+  }
+
+  async mensajeError(mensaje: string)
+  {
+    const a = await this.alert.create({
+      header    : 'Error',
+      subHeader : 'Mensaje de error',
+      message   : mensaje,
+      buttons   : ['Aceptar']
+    });
+    a.present();  
   }
 }
