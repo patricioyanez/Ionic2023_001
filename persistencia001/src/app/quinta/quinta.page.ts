36dc9ad0b217748998f3254aaab7bf7aedd4aba5
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-04 14:29:19 -0300

    update

--- persistencia001/src/app/quinta/quinta.page.ts
+++ persistencia001/src/app/quinta/quinta.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from './crud.service';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-quinta',
@@ -8,7 +11,9 @@ import { CrudService } from './crud.service';
 })
 export class QuintaPage implements OnInit {
   persona: any = [];
-  constructor(private crud: CrudService) { }
+  constructor(private crud: CrudService,
+    private toast: ToastController,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -16,8 +21,55 @@ export class QuintaPage implements OnInit {
   {
     // Ejercicio 2: Validar todos los datos  antes de guardar
     // usar TOAST rojo error y verde guardado.
-    await this.crud.guardar(this.persona.rut, this.persona);
+    if(!this.persona.rut)
+      this.mensajeError('Falta rut');
+    else if(!this.persona.nombre)
+      this.mensajeError('Falta nombre');
+    else if(!this.persona.direccion)
+      this.mensajeError('Falta Dirección');
+    else if(!this.persona.correo)
+      this.mensajeError('Falta Correo');
+    else if(!this.persona.telefono)
+      this.mensajeError('Falta teléfono');
+    else if(!this.persona.anioNacimiento)
+      this.mensajeError('Falta Año Nacimiento');
+    else
+    {
+      await this.crud.guardar(this.persona.rut, this.persona);
+      this.mensajeExito('Datos guardados');
+      this.persona = [];
+    }
 
     // Ejercicio 3: Terminar el crud para los datos de persona
+  }
+  listar()
+  {
+    this.router.navigateByUrl('quintalistado');
+  }
+  limpiar()
+  {
+    this.persona = [];
+  }
+  async mensajeError(mensaje: string)
+  {
+    const x = await this.toast.create({
+      message   : mensaje,
+      color     : 'danger',
+      icon      : 'alert-circle-outline',
+      duration  : 3000,
+      buttons   : ['Aceptar']
+    });
+    x.present();
+  }
+  async mensajeExito(mensaje: string)
+  {
+    const x = await this.toast.create({
+      message   : mensaje,
+      color     : 'success',
+      icon      : 'checkmark-circle-outline',
+      duration  : 3000,
+      buttons   : ['Aceptar']
+    })
+    x.present();
   }
 }
---
+++ persistencia001/src/app/quintalistado/quintalistado.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quintalistado',
+  templateUrl: './quintalistado.page.html',
+  styleUrls: ['./quintalistado.page.scss'],
+})
+export class QuintalistadoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
