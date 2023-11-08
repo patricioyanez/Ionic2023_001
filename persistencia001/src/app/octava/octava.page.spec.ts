ecf8317e16347bd31a53703c7233c7de4398763b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-08 14:01:41 -0300

    update

--- persistencia001/src/app/octava/octava.page.spec.ts
+++ persistencia001/src/app/octava/octava.page.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { OctavaPage } from './octava.page';
+import { IonicModule } from '@ionic/angular';
 
+/*
 describe('OctavaPage', () => {
   let component: OctavaPage;
   let fixture: ComponentFixture<OctavaPage>;
@@ -15,3 +17,22 @@ describe('OctavaPage', () => {
     expect(component).toBeTruthy();
   });
 });
+*/
+
+/* npm test / npm t */
+
+describe('Pagina sumadora',()=>{
+  let component: OctavaPage;
+  let fixture: ComponentFixture<OctavaPage>;
+
+  beforeEach(waitForAsync( async()=>{
+    await TestBed.configureTestingModule({
+      declarations: [OctavaPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(OctavaPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+})
